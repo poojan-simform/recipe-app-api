720a3406def79a39e4162912b916f8cd30f0c051
Poojan Patel <poojan.patel@simformsolutions.com>
2025-06-09 14:27:42 +0530

    Added user app and User API

--- app/app/settings.py
+++ app/app/settings.py
@@ -32,13 +32,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "core",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core",
+    "rest_framework",
+    "drf_spectacular",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +131,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "core.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
---
+++ app/user/views.py
@@ -0,0 +1,13 @@
+"""
+Views for the user API.
+"""
+
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user."""
+
+    serializer_class = UserSerializer
